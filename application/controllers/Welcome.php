ccd7f1e585c76fc39aedee37d9089af2ea3a0d18
kevinrsl <kevin.roussel.22800@gmail.com>
2023-12-06 09:10:14 +0100

    sbjqscjcsq

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,20 +4,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 
 	
-	 public function __construct(){
-		parent::__construct();
-		$this->load->model('internships_model');
-	}
-
 	public function index()
 	{
 		$this->load->view('header');
+		$this->load->view('accueil');
 	}
 
-	public function aide()
-	{
-		$this->load->view('aide');
-	}
 
 
 
